164c7bfa046cd9408c48af1507e78e0013fbc356
Gonzalo Garcia <alu0100403619@ull.edu.es>
2013-10-16 11:37:46 +0100

    Test de instanciación completado

--- racional.rb
+++ racional.rb
@@ -3,4 +3,17 @@
 require "./gcd.rb"
 
 class Fraccion
+  attr_reader :num, :den
+  
+  def initialize (num, den)
+    @num, @den = num, den
+  end
+  
+  def to_s
+    "#{@num}/#{@den}"
+  end
+  
 end
+
+#r1 = Fraccion.new(1, 2)
+#puts "r1 = #{r1}"
--- tc_racional.rb
+++ tc_racional.rb
@@ -5,6 +5,7 @@ require "test/unit"
 
 class Test_Fraccion < Test::Unit::TestCase
   def test_initialize
-    @r1 = Fraccion.new(1, 2)
+    #@r1 = Fraccion.new(1, 2)
+    assert_equal("1/2", Fraccion.new(1, 2).to_s)
   end
 end
